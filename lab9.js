e7147341328e8cebaba97fd101aced843b6642c7
hola <d.f9821n@gmail.com>
2023-04-16 11:59:45 -0500

    lab9

---
+++ lab9.js
@@ -0,0 +1,53 @@
+// Obtener el modal
+var modal = document.getElementById("modal-comment");
+
+// Obtener el botón que abre el modal
+var btn = document.getElementById("btn-comment");
+
+// Obtener el botón de cerrar
+var span = document.getElementsByClassName("close")[0];
+
+// Obtener el formulario de comentarios
+var commentForm = document.getElementById("comment-form");
+
+// Obtener la lista de comentarios
+var commentList = document.getElementById("comment-list");
+
+// Cuando se haga clic en el botón, abrir el modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// Cuando se haga clic en el botón de cerrar, cerrar el modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// Cuando el usuario haga clic fuera del modal, cerrarlo
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+
+// Cuando se envíe el formulario de comentarios
+commentForm.addEventListener("submit", function(event) {
+  event.preventDefault(); // Prevenir que se recargue la página
+
+  // Obtener los valores de los campos de formulario
+  let name = document.getElementById("name").value;
+  let comment = document.getElementById("comment").value;
+
+  // Crear el elemento de comentario
+  let commentElement = document.createElement("div");
+  commentElement.innerHTML = "<h3>" + name + "</h3><p>" + comment + "</p>";
+
+  // Agregar el elemento de comentario a la lista de comentarios
+  commentList.appendChild(commentElement);
+
+  // Cerrar el modal de comentarios
+  modal.style.display = "none";
+
+  // Limpiar los campos del formulario
+  commentForm.reset();
+});
